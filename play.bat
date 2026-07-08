a1a7c6f44d67b8fc33aac0f54508e899091237ef
SlmBdr <salambadar1@gmail.com>
2026-07-08 09:47:09 +0700

    fixing php directory and batch file directory

--- play.bat
+++ play.bat
@@ -1,3 +1,24 @@
 @echo off
-php "%~dp0play.php"
+set PHP_CMD=
+
+where php >nul 2>nul
+if %errorlevel% equ 0 set PHP_CMD=php
+if not "%PHP_CMD%"=="" goto RUN
+
+if exist "C:\xampp\php\php.exe" set PHP_CMD="C:\xampp\php\php.exe"
+if not "%PHP_CMD%"=="" goto RUN
+
+echo PHP tidak terdeteksi di PATH system Anda maupun folder default.
+set /p USER_PHP="Silakan ketik lokasi file php.exe Anda (contoh: C:\xampp\php\php.exe): "
+if not "%USER_PHP%"=="" set PHP_CMD="%USER_PHP%"
+
+if "%PHP_CMD%"=="" (
+    echo.
+    echo Error: Lokasi PHP tidak ditentukan. Program dibatalkan.
+    pause
+    exit /b
+)
+
+:RUN
+%PHP_CMD% "%~dp0play.php"
 pause
